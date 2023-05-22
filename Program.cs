b1f7bb876743421402e358ac9d613038394d184b
WunnaThaw <wunna.pe@gmail.com>
2023-05-22 20:19:25 +0630

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine("MyClass constructor is invoked");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine("Enter the class name:");
+        //string className = Console.ReadLine();
+
+        string className = "MyClass";
+
+        Type classType = Type.GetType(className)!;
+
+        if (classType != null)
+        {
+            object instance = Activator.CreateInstance(classType)!;
+            Console.WriteLine("Instance of {0} created", instance?.GetType().Name);
+        }
+        else
+        {
+            Console.WriteLine("Invalid class name");
+        }
+
+        Console.ReadKey();
+    }
+}
